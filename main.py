c36c9226d32a7db6b70fecd75944f85d869eb75d
Jasper van der Valk <jasper.vandervalk@channable.com>
2020-02-10 16:10:36 +0100

    add basic functionality

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import json
 import os
+import pprint
 import re
 
 
-test_data_path = r'./eigen_task/test_docs/'
-stored_data_path = r'./json_data/'
+TEST_DATA_PATH = r'./eigen_task/test_docs/'
+STORED_DATA_PATH = r'./json_data/'
 
 '''
 @dataclass
@@ -25,34 +26,60 @@ def get_file_paths(directory):
 
     return files
 
+def load_data():
+    list_of_data = []
+
+    for one_file in get_file_paths(TEST_DATA_PATH):
+        with open(file=one_file, mode='r', encoding='utf-8') as f:
+            list_of_data.append(
+                {os.path.splitext(os.path.basename(one_file))[0]: f.read()}
+            )
+    
+    return list_of_data
+
 def create_json_file(file_name, file_data):
     with open(file=file_name, mode='w', encoding='utf-8') as f:
         json.dump(file_data, f)
 
-def get_word_count(file_object):
+def get_word_count(list_of_data_dicts):
     word_count = {}
 
-    for word in f.read().split():
-        if word not in word_count:
-            word_count[word] = 1
-        else:
-            word_count[word] += 1
+    for data_dict in list_of_data_dicts:
+        for k, v in data_dict.items():
+            for word in v.split():
+                if word not in word_count:
+                    word_count[word] = 1
+                else:
+                    word_count[word] += 1
     return word_count
 
-def find_sentance_containing_word(data, word):
+def get_sentances_containing_word(data, word):
     search = r"([^.]*?" + word + r"[^.]*\.)"
     sentances = re.findall(search, data)
 
     return sentances
 
-def word_in_document(word, document):
+def word_in_document(word, documents):
     pass
 
-for one_file in get_file_paths(test_data_path):
-    file_name = stored_data_path + os.path.basename(one_file).split('.')[0] + '.json'
+data = load_data()
 
-    with open(file=one_file, mode='r', encoding='utf-8') as f:
-        # create_json_file(file_name, get_word_count(f))
-        
-        final_data = find_sentance_containing_word(f.read(), 'Let')
-        print(final_data)
+word_count = get_word_count(data)
+
+word = {
+    word: '',
+    appears_in: '',
+    count: 0,
+    sentances: ''
+    }
+
+for d in data:
+    for k, v in d.items():
+        print(k, get_sentances_containing_word(v, 'American'))
+
+
+
+'''
+if __name__ == '__main__':
+    paths = get_file_paths()
+'''
---
+++ test.py
@@ -0,0 +1,23 @@
+def read_files():
+    """
+    This function is used to read the files into memory.
+    """
+    pass
+
+def get_word_count():
+    """
+    This function counts the amount of times a word appears in a document or multiple documents.
+    """
+    pass
+
+def get_sentances_containing_word():
+    """
+    This function retrieves all the sentances which contain the specified word.
+    """
+    pass
+
+def get_docs_containing_word():
+    """
+    This function returns the documents which contain a given word.
+    """
+    pass
