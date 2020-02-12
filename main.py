0695b2e8ce379bc870828a3e2c9548cc64c73448
Jasper van der Valk <jasper.vandervalk@channable.com>
2020-02-12 10:42:49 +0100

    Add new functions for sorting on word count

--- main.py
+++ main.py
@@ -2,19 +2,20 @@ import json
 import os
 import pprint
 import re
+from dataclasses import dataclass
 
 
 TEST_DATA_PATH = r'./eigen_task/test_docs/'
 STORED_DATA_PATH = r'./json_data/'
 
-'''
+
 @dataclass
 class Word:
     word: str
     count: int
     sentances: list
     documents: list
-'''
+
 
 def get_file_paths(directory):
     files = []
@@ -27,31 +28,38 @@ def get_file_paths(directory):
     return files
 
 def load_data():
-    list_of_data = []
+    data = []
 
     for one_file in get_file_paths(TEST_DATA_PATH):
         with open(file=one_file, mode='r', encoding='utf-8') as f:
-            list_of_data.append(
+            data.append(
                 {os.path.splitext(os.path.basename(one_file))[0]: f.read()}
             )
     
-    return list_of_data
+    return data
 
-def create_json_file(file_name, file_data):
+def output_data_to_json_file(file_name, data):
     with open(file=file_name, mode='w', encoding='utf-8') as f:
-        json.dump(file_data, f)
+        json.dump(data, f)
 
-def get_word_count(list_of_data_dicts):
+def get_word_count(data):
     word_count = {}
 
-    for data_dict in list_of_data_dicts:
-        for k, v in data_dict.items():
+    for d in data:
+        for k, v in d.items():
             for word in v.split():
                 if word not in word_count:
                     word_count[word] = 1
                 else:
                     word_count[word] += 1
-    return word_count
+    
+    sorted_word_count = sorted(
+        word_count.items(), 
+        key=lambda x: x[1], 
+        reverse=True
+    )
+
+    return sorted_word_count
 
 def get_sentances_containing_word(data, word):
     search = r"([^.]*?" + word + r"[^.]*\.)"
@@ -62,24 +70,9 @@ def get_sentances_containing_word(data, word):
 def word_in_document(word, documents):
     pass
 
+
 data = load_data()
 
-word_count = get_word_count(data)
+word_count_data = get_word_count(data)
 
-word = {
-    word: '',
-    appears_in: '',
-    count: 0,
-    sentances: ''
-    }
-
-for d in data:
-    for k, v in d.items():
-        print(k, get_sentances_containing_word(v, 'American'))
-
-
-
-'''
-if __name__ == '__main__':
-    paths = get_file_paths()
-'''
+print(word_count_data)
